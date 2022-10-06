67a573846d3900629f50a8d9368270f43ddd48f6
Mariusz Malec <mario106c@gmail.com>
2022-10-06 19:56:36 +0200

    added read from appsettings.json serilog options

--- WepAppAccessToApi/Program.cs
+++ WepAppAccessToApi/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Net.Http.Headers;
+using Serilog;
 using WepAppAccessToApi.Areas.Identity.Data;
 using WepAppAccessToApi.Data;
 using WepAppAccessToApi.Middleware;
@@ -9,6 +10,20 @@ var builder = WebApplication.CreateBuilder(args);
 var connectionString = builder.Configuration.GetConnectionString("WepAppAccessToApiContextConnection");builder.Services.AddDbContext<WepAppAccessToApiContext>(options =>
     options.UseSqlite(connectionString));builder.Services.AddDefaultIdentity<WepAppAccessToApiUser>(options => options.SignIn.RequireConfirmedAccount = false)
     .AddEntityFrameworkStores<WepAppAccessToApiContext>();
+
+
+var configuration = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json")
+    .AddUserSecrets<Program>(optional: true)
+    .AddEnvironmentVariables()
+    .Build();
+
+Log.Logger = new LoggerConfiguration()
+    .ReadFrom.Configuration(configuration)
+    .CreateLogger();
+
+Log.Logger.Information("Start Application ...");
+
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
